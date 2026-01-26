db5ab6b11cf6aae6982560d0dc8f2411210f27aa
ponda <bashvitzben@gmail.com>
2026-01-26 12:33:44 +0200

    feature: add route to get all comments

--- src/routes/commentRoutes.ts
+++ src/routes/commentRoutes.ts
@@ -5,4 +5,6 @@ const router = express.Router();
 
 router.post("/", commentController.post.bind(commentController));
 
+router.get("/", commentController.getAll.bind(commentController));
+
 export default router;
