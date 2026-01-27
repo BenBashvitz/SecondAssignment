d45701c508ea361fd0b494bda95e294f4908e3be
ponda <bashvitzben@gmail.com>
2026-01-27 15:03:57 +0200

    fix: remove unnecessary changes

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -11,6 +11,4 @@ router.get("/:id", postController.getById.bind(postController));
 
 router.put("/:id", postController.put.bind(postController));
 
-router.delete("/:id", postController.delete.bind(postController));
-
 export default router;
