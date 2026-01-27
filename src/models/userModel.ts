f96c5e5361e29c67200ce003521ed7e742e60fa4
Noam Shimoni <noamsh9103@cs.colman.ac.il>
2026-01-27 19:05:43 +0200

    fix username typo

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema<User>({
     required: true,
     unique: true,
   },
-  userName: {
+  username: {
     type: String,
     required: true,
   },
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,5 +1,5 @@
 export type User = {
   email: string;
-  userName: string;
+  username: string;
   password: string;
 };
