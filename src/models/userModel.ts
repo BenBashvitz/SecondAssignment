85203e717ea45f5eebd3635c37379126e7c22a63
Noam Shimoni <noamsh9103@cs.colman.ac.il>
2026-01-27 19:04:00 +0200

    adding userName to userModel

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -7,6 +7,10 @@ const userSchema = new mongoose.Schema<User>({
     required: true,
     unique: true,
   },
+  userName: {
+    type: String,
+    required: true,
+  },
   password: {
     type: String,
     required: true,
