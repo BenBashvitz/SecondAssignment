396d17c5470d0a7dd7ddc2f06f65026f6c3b1963
Noam Shimoni <noamsh9103@cs.colman.ac.il>
2026-01-27 19:04:47 +0200

    remove redundant files

--- tests/consts.ts
+++
@@ -1,6 +0,0 @@
-import Movie from "../types/movie";
-
-export const MOVIES: Omit<Movie, "createdBy">[] = [
-  { title: "inception", releaseYear: 2010 },
-  { title: "inception2", releaseYear: 2015 },
-];
