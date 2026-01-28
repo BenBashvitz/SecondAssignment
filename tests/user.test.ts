c248b3f4a69ecd0e2196461c008d327452b6faed
Noam Shimoni <noamsh9103@cs.colman.ac.il>
2026-01-28 22:15:15 +0200

    adding user creation route

---
+++ tests/user.test.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import initApp from "../src/index";
+import userModel from "../src/models/userModel";
+import request from "supertest";
+import { Express } from "express";
+import { USERS } from "./consts";
+
+let app: Express;
+
+beforeAll(async () => {
+  app = await initApp();
+
+  await userModel.deleteMany();
+});
+
+describe("User Model Test Suite", () => {
+  it("should create and save a user successfully", async () => {
+    for (const user of USERS) {
+      const response = await request(app).post("/user").send(user);
+
+      expect(response.status).toBe(201);
+      expect(response.body).toMatchObject(user);
+    }
+  });
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
