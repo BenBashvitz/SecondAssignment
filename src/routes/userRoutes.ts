040c533be81743ef41e4c84d09f9c03b4e8b850c
Noam Shimoni <noamsh9103@cs.colman.ac.il>
2026-01-29 23:04:15 +0200

    fix response indents

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -36,12 +36,12 @@ const router = express.Router();
  *           application/json:
  *             schema:
  *               $ref: '#/components/schemas/User'
- *      500:
- *        description: Server error
- *       content:
- *        application/json:
- *         schema:
- *          $ref: '#/components/schemas/Error'
+ *       500:
+ *         description: Server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
  */
 router.post("/", userController.post.bind(userController));
 
