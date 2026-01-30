6249511af3f0a00eb6173b145957254abbc96aa3
Noam Shimoni <noamsh9103@cs.colman.ac.il>
2026-01-30 20:17:21 +0200

    adding login route with tests and swagger doc

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -51,6 +51,45 @@ const register = async (req: Request, res: Response) => {
   }
 };
 
+const login = async (req: Request, res: Response) => {
+  const { email, password, username } = req.body;
+
+  if (!email || !password || !username) {
+    return res
+      .status(400)
+      .json({ message: "email, password, and username are required." });
+  }
+
+  try {
+    const user = await userModel.findOne({ email, username });
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "Invalid email, username or password." });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res
+        .status(401)
+        .json({ message: "Invalid email, username or password." });
+    }
+
+    const tokens = generateTokens(user._id.toString());
+
+    await user.save();
+
+    return res.status(200).json(tokens);
+  } catch (error) {
+    console.error("Login error: ", error);
+
+    return res.status(500).json({ message: "Error logging in." });
+  }
+};
+
 export default {
   register,
+  login,
 };
